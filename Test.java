ccaecc5b9b00e72a4115d86102e462df02d42c5b
Sreshta Alwis <alwissreshta@gmail.com>
2026-05-20 14:13:47 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test{
 
       public static void main (String[] args){
 	  
-           System.out.println("Hello World!");
+           System.out.println("Hello GitHub! This is my first repository.");
 	}
 }
